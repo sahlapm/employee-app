a4aec4b6a5d6754b397e6ebd482d98f718315fda
Sahla <sahlapm@gmail.om>
2023-02-05 19:29:20 +0300

    commit

--- employee-backend/app.js
+++ employee-backend/app.js
@@ -21,7 +21,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname,'/build')));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname,'/build/index.html')); });
-
+*/
     app.use('/api/employee',employee);
     app.use('/api/user',user);
     
